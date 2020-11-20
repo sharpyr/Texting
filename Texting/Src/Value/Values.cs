1485919ddc4db0264f8ee7ef3ed9fd7489040ccd
hazen <hoyeungw@outlook.com>
2020-11-20 11:00:48 +0800

    add Texting.Value

---
+++ Texting/Src/Value/Values.cs
@@ -0,0 +1,9 @@
+namespace Texting.Value {
+  public static class Values {
+    public static int V1(string word) => 0;
+    public static int V2(string word) => 0;
+    public static int V3(string word) => 0;
+    public static int V4(string word) => 0;
+    public static int StrValue(this string word) => 0;
+  }
+}
