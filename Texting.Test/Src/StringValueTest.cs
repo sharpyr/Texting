d6e445057bfe4417d93a3f4f14d297148a5ae477
hoyeungw <hoyeungw@outlook.com>
2021-01-29 14:30:08 +0800

    minor fix

---
+++ Texting.Test/Src/StringValueTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using Spare.Logger;
+using Texting.Value;
+using Veho.Vector;
+
+namespace Texting.Test {
+  [TestFixture]
+  public class StringValueTests {
+    [Test]
+    public void StringValueTest() {
+      var texts = new[] {
+        "Warren",
+        "WSJ",
+        "GlobalTimes",
+        "ZZZZ",
+        "zzzz",
+        "MetalGear 1",
+        "MetalGear 2",
+        "123",
+        "abc",
+        "---",
+        "",
+      };
+
+      texts.Iterate(x => {
+        $"{x}: {x.StringValue()}".Logger();
+        // $"{x}: {(x[1] & 0x7f) << 14}".Logger();
+      });
+    }
+  }
+}
--- Texting/Src/Value/Values.cs
+++ Texting/Src/Value/Values.cs
@@ -1,9 +1,30 @@
+using Texting.Slices;
+
 namespace Texting.Value {
   public static class Values {
-    public static int V1(string word) => 0;
-    public static int V2(string word) => 0;
-    public static int V3(string word) => 0;
-    public static int V4(string word) => 0;
-    public static int StrValue(this string word) => 0;
+    public static int V1(this string word) => (word.ToLower()[0] & 0x7f) << 21;
+    public static int V2(this string word) => (((word = word.ToLower())[0] & 0x7f) << 21) +
+                                              ((word[1] & 0x7f) << 14);
+    public static int V3(this string word) => (((word = word.ToLower())[0] & 0x7f) << 21) +
+                                              ((word[1] & 0x7f) << 14) +
+                                              ((word[2] & 0x7f) << 7);
+    public static int V4(this string word) => (((word = word.ToLower())[0] & 0x7f) << 21) +
+                                              ((word[1] & 0x7f) << 14) +
+                                              ((word[2] & 0x7f) << 7) +
+                                              (word[3] & 0x7f);
+    public static double StringValue(this string word) {
+      var l = word.Length;
+      if (l >= 8) return (V4(word.Pre(4)) << 2) + V4(word.Slice(-4));
+      switch (l) {
+        case 7: return (V4(word.Pre(4)) << 2) + V3(word.Slice(-3));
+        case 6: return (V4(word.Pre(4)) << 2) + V2(word.Slice(-2));
+        case 5: return (V4(word.Pre(4)) << 2) + V1(word.Slice(-1));
+        case 4: return V4(word) << 2;
+        case 3: return V3(word) << 2;
+        case 2: return V2(word) << 2;
+        case 1: return V1(word) << 2;
+        default: return double.NaN;
+      }
+    }
   }
 }
