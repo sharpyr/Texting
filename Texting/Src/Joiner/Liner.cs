61229a2fc58ccc6b792678059b86e17f57d30090
hoyeungw <hoyeungw@outlook.com>
2021-08-16 10:23:14 +0800

    add Join, JoinLines, JoinLinesEdge, ContingentLines

---
+++ Texting/Src/Joiner/Liner.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using Texting.Bracket;
+using Texting.Enums;
+using Ch = Texting.Enums.Chars;
+using Texting.Repeat;
+using static Texting.Enums.Strings;
+
+namespace Texting.Joiner {
+  public static class Joiners {
+    public static string Join(this string[] words, string delim) {
+      var hi = words.Length;
+      if (hi == 0) return "";
+      var phrase = words[0];
+      for (var i = 1; i < hi; i++) phrase += delim + words[i];
+      return phrase;
+    }
+    public static string JoinLines(this string[] lines, string delim = ",", byte level = 0) {
+      var ind = level > 0 ? Ch.SP.Repeat(level << 1) : "";
+      return $"{ind + TB}{lines.Join(delim + LF + ind + TB)}{delim}";
+    }
+    public static string JoinLinesEdged(this string[] lines, string delim = ",", int level = 0) {
+      var ind = level > 0 ? Ch.SP.Repeat(level << 1) : "";
+      return $"{LF + ind + TB}{lines.Join(delim + LF + ind + TB)}{delim + LF + ind}";
+    }
+
+    public static readonly Regex LINEFEED = new Regex("\n", RegexOptions.Compiled);
+    public static readonly Regex COMMA = new Regex(",", RegexOptions.Compiled);
+
+    public static string ContingentLines(
+      this string[] lines,
+      string delim = LF,
+      byte level = 0,
+      Brac brac = Brac.NAN
+    ) {
+      var edged = brac != Brac.NAN;
+      var joined = lines.Any() && LINEFEED.IsMatch(delim)
+        ? edged
+          ? lines.JoinLinesEdged(COMMA.IsMatch(delim) ? CO : "", level)
+          : lines.JoinLines(COMMA.IsMatch(delim) ? CO : "", level)
+        : lines.Join(delim);
+      return joined.Br(brac);
+    }
+  }
+}
