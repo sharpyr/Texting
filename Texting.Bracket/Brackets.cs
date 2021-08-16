960de6507e552f8c142d182a26dbe0e0c2d79feb
hoyeungw <hoyeungw@outlook.com>
2021-08-16 10:22:27 +0800

    add Br

--- Texting.Bracket/Brackets.cs
+++ Texting.Bracket/Brackets.cs
@@ -1,8 +1,20 @@
-﻿namespace Texting.Bracket {
+﻿using Texting.Enums;
+
+namespace Texting.Bracket {
   public static class Brackets {
     public static string Parenth(this string x) => "(" + x + ")";
     public static string Bracket(this string x) => "[" + x + "]";
     public static string Brace(this string x) => "{" + x + "}";
     public static string AngleBr(this string x) => "<" + x + ">";
+    public static string Br(this string text, Brac brac) {
+      switch (brac) {
+        case Brac.PAR: return "(" + text + ")";
+        case Brac.BRK: return "[" + text + "]";
+        case Brac.BRC: return "{" + text + "}";
+        case Brac.ANG: return "<" + text + ">";
+        case Brac.NAN: return text;
+        default: return text;
+      }
+    }
   }
 }
