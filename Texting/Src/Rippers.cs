54adf06072480c341d8ce17accbf5146607ae581
hoyeungw <hoyeungw@outlook.com>
2021-09-27 09:22:25 +0800

    add Ripper

---
+++ Texting/Src/Rippers.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Texting {
+  public static class Rippers {
+    public static List<string> Ripper(this Regex reg, string text) {
+      var vec = new List<string>();
+      int l = 0, r = 0;
+      string space;
+      var match = reg.Match(text);
+      while (match.Success) {
+        r = match.Index;
+        // Console.WriteLine($">> [match] ({match.Value}) [l] {l} [r] {r}");
+        if ((space = text.Slice(l, r - l)).Length > 0) vec.Add(space);
+        vec.Add(match.Value);
+        l = r + match.Length;
+        match = match.NextMatch();
+      }
+      if (l < text.Length) vec.Add(text.Slice(l));
+      return vec;
+    }
+  }
+}
